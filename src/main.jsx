5b01afa8ba5e4fe4b4be9af962470eed067e7114
Alva010 <kaungkhant1107@gmail.com>
2024-05-18 09:17:10 -0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -9,17 +9,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   </React.StrictMode>,
 );
 
-
-// src/main.jsx or src/index.js
-// import React from 'react';
-// // import ReactDOM from 'react-dom';
-// import App from './App';
-// import './index.css'; // Import the Tailwind CSS file
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
